587b2d49816f8b9034f21924364f01ff6beb4c9d
dh1010a <dh1010a@naver.com>
2026-08-08 13:13:31 +0900

    [NOOK-125] 저장 장소 검색 API 추가

--- nook-api-infrastructure/src/main/kotlin/org/every/nook/api/infrastructure/config/PlaceMapQueryUseCaseConfig.kt
+++ nook-api-infrastructure/src/main/kotlin/org/every/nook/api/infrastructure/config/PlaceMapQueryUseCaseConfig.kt
@@ -2,7 +2,11 @@ package org.every.nook.api.infrastructure.config
 
 import org.every.nook.api.application.place.GetMapPlacesUseCase
 import org.every.nook.api.application.place.GetRecentPlacesUseCase
+import org.every.nook.api.application.place.SearchAllStoredPlacesUseCase
+import org.every.nook.api.application.place.SearchMyStoredPlacesUseCase
 import org.every.nook.api.application.place.port.PlaceMapQueryPort
+import org.every.nook.api.application.place.port.SearchAllStoredPlacesPort
+import org.every.nook.api.application.place.port.SearchMyStoredPlacesPort
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -15,4 +19,12 @@ class PlaceMapQueryUseCaseConfig {
     @Bean
     fun getRecentPlacesUseCase(placeMapQueryPort: PlaceMapQueryPort): GetRecentPlacesUseCase =
         GetRecentPlacesUseCase(placeMapQueryPort)
+
+    @Bean
+    fun searchAllStoredPlacesUseCase(port: SearchAllStoredPlacesPort): SearchAllStoredPlacesUseCase =
+        SearchAllStoredPlacesUseCase(port)
+
+    @Bean
+    fun searchMyStoredPlacesUseCase(port: SearchMyStoredPlacesPort): SearchMyStoredPlacesUseCase =
+        SearchMyStoredPlacesUseCase(port)
 }
