b59be2810317cdafd081cf2a57bbbcc9461d2d89
dh1010a <dh1010a@naver.com>
2026-09-16 22:54:32 +0900

    [NOOK-306] 아카이브 최근 저장순 정렬

--- nook-api-infrastructure/src/test/kotlin/org/every/nook/api/infrastructure/persistence/group/GroupJpaRepositoryQueryTest.kt
+++ nook-api-infrastructure/src/test/kotlin/org/every/nook/api/infrastructure/persistence/group/GroupJpaRepositoryQueryTest.kt
@@ -16,4 +16,16 @@ class GroupJpaRepositoryQueryTest {
         assertContains(query, "WHEN post_media.media_type = 'VIDEO' THEN post_media.thumbnail_url")
         assertContains(query, "post_media.media_type = 'VIDEO' AND post_media.thumbnail_url IS NOT NULL")
     }
+
+    @Test
+    fun `groups are ordered by their most recent saved post then creation time`() {
+        val query = GroupJpaRepository::class.java
+            .getMethod("findAllSummaries", Long::class.javaPrimitiveType)
+            .getAnnotation(Query::class.java)
+            .value
+
+        assertContains(query, "MAX(group_post.updated_at) IS NULL ASC")
+        assertContains(query, "MAX(group_post.updated_at) DESC")
+        assertContains(query, "user_group.created_at DESC")
+    }
 }
