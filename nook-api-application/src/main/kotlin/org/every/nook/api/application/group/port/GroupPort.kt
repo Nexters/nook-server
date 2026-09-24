d41599c32fa4b48572a0c815ffa99cc1096e25ee
Kijun Kwon <39583312+kkjsw17@users.noreply.github.com>
2026-09-24 23:56:49 +0900

    [NOOK-331] 저장 가능한 내 아카이브 조회 API 추가 (#235)

--- nook-api-application/src/main/kotlin/org/every/nook/api/application/group/port/GroupPort.kt
+++ nook-api-application/src/main/kotlin/org/every/nook/api/application/group/port/GroupPort.kt
@@ -6,6 +6,8 @@ import org.every.nook.api.domain.group.GroupColor
 interface GroupPort {
     fun findAll(userId: Long): List<GroupView>
 
+    fun findOwned(userId: Long): List<GroupView>
+
     fun create(userId: Long, name: String, color: GroupColor): GroupView
 
     fun update(userId: Long, groupId: Long, name: String, color: GroupColor): UpdateResult
